69db0ce70a1153ec10cc4308af2412a751e0a296
Rizal KA <112916824+rizalkhoirula@users.noreply.github.com>
2023-06-30 22:03:37 +0700

    update email php

--- email.php
+++ email.php
@@ -1,34 +1,16 @@
 <?php
+ if(!empty ($_POST["send" ])){
 $name = $_POST["name"];
 $email = $_POST["email"];
 $subject = $_POST["subject"];
 $message = $_POST["message"];
-require "vendor/autoload.php";
-
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
-
-$mail = new PHPMailer(true);
-
-// $mail->SMTPDebug = SMTP::DEBUG_SERVER;
-
-$mail->isSMTP();
-$mail->SMTPAuth = true;
-
-$mail->Host = "smtp.example.com";
-$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-$mail->Port = 587;
-
-$mail->Username = "you@example.com";
-$mail->Password = "password";
-
-$mail->setFrom($email, $name);
-$mail->addAddress("dave@example.com", "Dave");
-
-$mail->Subject = $subject;
-$mail->Body = $message;
-
-$mail->send();
-
-header("Location: sent.html");
+$toEmail = $_POST[ "rrizalkaa@gmail.com"];
+$mailHeaders= "Name: " . $name .
+"\r\n Email:" . $email .
+"\r\n Subject:" . $subject .
+"\r\n Message:" . $message . "\r\n";
+    if(mail($toEmail, $name, $mailHeaders)){
+$result = "Your message is received successfully.";
+    }
+}
 ?>
